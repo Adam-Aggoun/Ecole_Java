863a1d18cb3ab05c2e0d68aa7a878687cd0938f7
Adam-Aggoun <a.aggun@lprs.fr>
2021-01-26 16:01:42 +0100

    Lien entre les pages index, inscription et connexion

--- ecole_java/src/view/page_connexion.java
+++ ecole_java/src/view/page_connexion.java
@@ -10,7 +10,7 @@ import java.awt.Font;
 
 public class page_connexion {
 
-	private JFrame frame;
+	public JFrame page_connexion;
 	private JTextField textField;
 	private JTextField textField_1;
 
@@ -22,7 +22,7 @@ public class page_connexion {
 			public void run() {
 				try {
 					page_connexion window = new page_connexion();
-					window.frame.setVisible(true);
+					window.page_connexion.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -38,36 +38,36 @@ public class page_connexion {
 	}
 
 	/**
-	 * Initialize the contents of the frame.
+	 * Initialize the contents of the page_connexion.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 390, 301);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		page_connexion = new JFrame();
+		page_connexion.setBounds(100, 100, 390, 301);
+		page_connexion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		page_connexion.getContentPane().setLayout(null);
 		
 		textField = new JTextField();
 		textField.setBounds(146, 27, 149, 20);
-		frame.getContentPane().add(textField);
+		page_connexion.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Identifiant");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel.setBounds(10, 23, 96, 21);
-		frame.getContentPane().add(lblNewLabel);
+		page_connexion.getContentPane().add(lblNewLabel);
 		
 		JButton btnNewButton = new JButton("Connexion");
 		btnNewButton.setBounds(129, 185, 89, 23);
-		frame.getContentPane().add(btnNewButton);
+		page_connexion.getContentPane().add(btnNewButton);
 		
 		JLabel lblNewLabel_1 = new JLabel("Mot de passe");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_1.setBounds(10, 95, 126, 21);
-		frame.getContentPane().add(lblNewLabel_1);
+		page_connexion.getContentPane().add(lblNewLabel_1);
 		
 		textField_1 = new JTextField();
 		textField_1.setBounds(146, 96, 149, 20);
-		frame.getContentPane().add(textField_1);
+		page_connexion.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 	}
 }
